042318c7af7c2de0c5fa922e69067ed4c0ec98aa
nilsnils191 <nilsnils191@gmail.com>
2019-12-15 23:25:58 +0530

    Config server and config client empty projects without actual implementation.

---
+++ config-client-sample/src/main/java/com/cloud/config/configclientsample/ConfigClientSampleApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.config.configclientsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientSampleApplication.class, args);
+	}
+
+}
---
+++ config-server-nilesh/src/test/java/com/cloud/config/configservernilesh/ConfigServerNileshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.config.configservernilesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerNileshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
