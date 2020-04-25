12936daf14b64433e1fa8fe6b1e1f9ef3d88d8e5
nilsnils191 <nilsnils191@gmail.com>
2020-04-25 23:44:26 +0530

    refresh scope added for spring cloud config client

--- config-client-sample/src/main/java/com/cloud/config/configclientsample/controller/ClientControllerSample.java
+++ config-client-sample/src/main/java/com/cloud/config/configclientsample/controller/ClientControllerSample.java
@@ -1,12 +1,14 @@
 package com.cloud.config.configclientsample.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+@RefreshScope
 public class ClientControllerSample {
 
 	@Value("${client.test.message: Default Message}")
